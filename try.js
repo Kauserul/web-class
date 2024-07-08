965decd33b0eb94cd759569e38489930c53ea515
MD. Kauserul Islam <kauserulisalm0055@gmail.com>
2024-07-08 19:35:48 +0600

    Tomi onek bora hoba!!

---
+++ try.js
@@ -0,0 +1,20 @@
+// numerice variable
+let number = 22;
+
+// string variable
+let name = "random"
+
+//boolen variable
+let isHappy = true;
+
+
+function totaNum (){
+    let a =10;
+    let b = 5;
+
+    let sum = a + b;
+
+    console.log(sum);
+}
+
+totaNum();
